2d83f49e71b1493f42c414dbb10c58b050a9e659
Frez7373 <danilla.mikhailov@gmail.com>
2025-04-05 00:50:38 +0300

    Update clock.lua

--- clock.lua
+++ clock.lua
@@ -35,6 +35,31 @@ local function startTimer(duration)
   sleep(2)
 end
 
+local function getTimerDuration()
+  term.clear()
+  term.setCursorPos(1,1)
+  print("Set timer duration (in seconds):")
+  local duration = 0
+  while true do
+    term.setCursorPos(1,3)
+    term.clearLine()
+    print("Duration: " .. duration .. " seconds")
+    local event, _, _, _, _ = os.pullEventRaw()
+    if event == "mouse_click" then
+      return duration
+    elseif event == "key" then
+      if _ == keys.up then
+        duration = duration + 1
+      elseif _ == keys.down then
+        if duration > 0 then
+          duration = duration - 1
+        end
+      end
+    end
+    sleep(0.1)
+  end
+end
+
 print("== Clock ==")
 print("Tap screen to return.")
 
@@ -44,7 +69,7 @@ while true do
   term.clearLine()
   print("Time: " .. timeStr)
   print("1 - Start Stopwatch")
-  print("2 - Start Timer (10 sec)")
+  print("2 - Start Timer")
 
   local event, _, _, x, y = os.pullEventRaw()
 
@@ -52,7 +77,8 @@ while true do
     if x >= 1 and x <= 1 + 22 and y >= 5 and y <= 5 then -- Stopwatch click area
       startStopwatch()
     elseif x >= 1 and x <= 1 + 22 and y >= 6 and y <= 6 then -- Timer click area
-      startTimer(10) -- 10 seconds timer
+      local duration = getTimerDuration()
+      startTimer(duration)
     else
       break
     end
